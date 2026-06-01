18c60d69bccbe4056f9aa9ed8377409c76aee12b
Wayfarer <96461983+ziqi108@users.noreply.github.com>
2026-06-01 12:54:26 +0800

    Delete app/Learn directory

--- app/Learn/advanced/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdvancedPage() {
-  return (
-    <div>
-      <h1>Advanced Learning</h1>
-    </div>
-  );
-}
--- app/Learn/intermediate/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function IntermediatePage() {
-  return (
-    <div>
-      <h1>Intermediate Learning</h1>
-    </div>
-  );
-}
